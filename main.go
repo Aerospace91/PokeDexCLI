46c9c6b01e138786efcccbbfaef21d7fe0cb9f4c
Aerospace91 <beenters@gmail.com>
2026-07-28 13:57:15 -0700

    Adding locations

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"time"
+
+	"github.com/Aerospace91/PokeDexCLI/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+	startRepl(cfg)
 }
