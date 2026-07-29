50afbd70825e98fad9b7fc64db79b84153741ef0
Aerospace91 <beenters@gmail.com>
2026-07-29 09:18:22 -0700

    Cleanup

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,39 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+// Cache -
+type Cache struct {
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+// NewCache -
+func NewCache(interval, time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
+	}
+
+	go c.reapLoop(interval)
+
+	return c
+}
+
+// Add -
+func (c *Cache) Add(key string, value []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       value,
+	}
+}
